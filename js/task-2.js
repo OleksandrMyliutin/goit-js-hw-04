eb25a471237bb465dfaa1578c28839916ff8ff4d
Oleksandr Mil <oleksandr.myliutin@gmail.com>
2024-12-21 20:15:49 +0200

    add second task

---
+++ js/task-2.js
@@ -0,0 +1,43 @@
+function calcAverageCalories(days) {
+    const valuesObject = Object.values(days);
+    
+    let counter = 0;
+    for (const value of valuesObject) {
+        counter += value.calories;
+    }
+    let result = 0;
+    if (valuesObject.length < 1) {
+        return result;
+    } else {
+        return result = counter/ valuesObject.length;
+    }
+
+}
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+  ])
+); // 2270
+
+console.log(
+  calcAverageCalories([])
+); // 0
